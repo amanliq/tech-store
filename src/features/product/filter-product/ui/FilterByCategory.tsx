7364a59d841aaeed527a889173e1023e656d8984
n.amanliq <nurimbetov4433@gmail.com>
2026-02-03 21:56:56 +0500

    feat(product): add filter-by-category logic

--- src/features/product/filter-product/ui/FilterByCategory.tsx
+++ src/features/product/filter-product/ui/FilterByCategory.tsx
@@ -1,3 +1,26 @@
-export const FilterByCategory = () => {
-  return <div>FilterByCategory</div>;
+import { CATEGORY_OPTIONS } from "@/entities/product";
+import { Button } from "@/shared/ui";
+
+interface Props {
+  currentCategory: string;
+  onCategoryChange: (category: string) => void;
+}
+
+export const FilterByCategory = ({
+  currentCategory,
+  onCategoryChange,
+}: Props) => {
+  return (
+    <div className="flex gap-4 mb-8">
+      {CATEGORY_OPTIONS.map((cat) => (
+        <Button
+          key={cat.value}
+          onClick={() => onCategoryChange(cat.value)}
+          variant={currentCategory === cat.value ? "default" : "secondary"}
+        >
+          {cat.label}
+        </Button>
+      ))}
+    </div>
+  );
 };
