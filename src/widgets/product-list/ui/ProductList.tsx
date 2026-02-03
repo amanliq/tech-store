11060e1d9b02a6a2e8bf2ef606a5e52406a9683c
n.amanliq <nurimbetov4433@gmail.com>
2026-02-03 16:06:08 +0500

    connect product-list with loading and error component

--- src/widgets/product-list/ui/ProductList.tsx
+++ src/widgets/product-list/ui/ProductList.tsx
@@ -4,12 +4,25 @@ import {
   type Category,
 } from "@/entities/product";
 import { Link } from "react-router-dom";
+import ProductListLoading from "./ProductListLoading";
+import ProductListError from "./ProductListError";
 
 interface ProductListProps {
   category?: Category;
 }
 export const ProductList = ({ category }: ProductListProps) => {
-  const { data: products } = useProductsQuery(category);
+  const {
+    data: products,
+    isLoading,
+    error,
+    isError,
+  } = useProductsQuery(category);
+
+  if (isLoading) return <ProductListLoading />;
+  if (isError)
+    return (
+      <ProductListError error={error?.message ?? "Error loading products."} />
+    );
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
