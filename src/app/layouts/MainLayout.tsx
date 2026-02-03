178767656df27e8c05fc0a1e7d7b22cc21295f60
n.amanliq <nurimbetov4433@gmail.com>
2026-02-03 13:28:47 +0500

    add main layout and connected to router

---
+++ src/app/layouts/MainLayout.tsx
@@ -0,0 +1,16 @@
+import { Header } from "@/widgets";
+import { Outlet } from "react-router-dom";
+
+export const MainLayout = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <footer className="py-6 text-center text-sm text-gray-400 border-t bg-white">
+        © 2026 Tech Store.
+      </footer>
+    </div>
+  );
+};
--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,5 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { MainLayout } from "../layouts/MainLayout";
 
-const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+  },
+]);
 
 export const AppRouter = () => <RouterProvider router={router} />;
