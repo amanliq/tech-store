1c4b3bcc8a3ea45d4bf38aec4f8d76da86151786
n.amanliq <nurimbetov4433@gmail.com>
2026-02-03 15:21:50 +0500

    add prouduct queries

---
+++ src/entities/product/api/queries.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchProducts, fetchProductById } from "./productApi";
+
+export const productKeys = {
+  all: ["products"] as const,
+
+  list: (category?: string) => [productKeys.all, { category }] as const,
+
+  detail: (id: string | number) => [...productKeys.all, "detail", id] as const,
+};
+
+export const useProductsQuery = (category?: string) => {
+  return useQuery({
+    queryKey: productKeys.list(category),
+    queryFn: () => fetchProducts(category),
+    staleTime: 60 * 1000,
+  });
+};
+
+export const useProductByIdQuery = (id: string | number) => {
+  return useQuery({
+    queryKey: productKeys.detail(id),
+    queryFn: () => fetchProductById(id),
+    enabled: !!id,
+    staleTime: 60 * 1000,
+  });
+};
