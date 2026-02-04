c07144417968248795b0577941109d61fdf6ba49
n.amanliq <nurimbetov4433@gmail.com>
2026-02-04 12:26:22 +0500

    feat: connect auth-provider to App root

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,13 @@
+import { AuthProvider } from "./providers/AuthProvider";
 import { QueryProvider } from "./providers/QueryProvider";
 import { AppRouter } from "./router";
 
 function App() {
   return (
     <QueryProvider>
-      <AppRouter />;
+      <AuthProvider>
+        <AppRouter />;
+      </AuthProvider>
     </QueryProvider>
   );
 }
