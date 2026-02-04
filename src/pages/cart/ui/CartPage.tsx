6152cf07566ab026a6061a7168d7994469a3d361
n.amanliq <nurimbetov4433@gmail.com>
2026-02-04 16:33:26 +0500

    feat: add cart summary

--- src/pages/cart/ui/CartPage.tsx
+++ src/pages/cart/ui/CartPage.tsx
@@ -1,10 +1,17 @@
-import { CartList } from "@/widgets/cart";
+import { CartList, CartSummary } from "@/widgets/cart";
 
 export const CartPage = () => {
   return (
     <div className="my-7">
       <div className="container mx-auto">
-        <CartList />
+        <div className="flex w-full gap-4">
+          <div className="flex-1">
+            <CartList />
+          </div>
+          <div className="w-md ">
+            <CartSummary />
+          </div>
+        </div>
       </div>
     </div>
   );
